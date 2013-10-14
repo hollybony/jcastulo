3b9435ab18d2d620636d8dff7848bc2b94bf1c0c
Carlos <Carlos@Carlos-PC>
2013-10-14 12:06:27 -0400

    frame properties added

--- src/main/java/caja/jcastulo/media/FrameIterator.java
+++ src/main/java/caja/jcastulo/media/FrameIterator.java
@@ -2,9 +2,10 @@ package caja.jcastulo.media;
 
 import caja.jcastulo.media.entities.AudioMedia;
 import java.io.IOException;
+import java.util.Map;
 
 /**
- * Iterates over a resource that provides frames
+ * Iterates over a audio media that can be sliced up in frames
  * 
  * @author Carlos Juarez
  */
@@ -14,24 +15,25 @@ public interface FrameIterator {
      * Returns true if this reader supports the format of the given media.
      *
      * @param media - media to analyze
+     * @param properties - other properties requested to read the media
      * @return <code>true</code> if the media is supported
      */
-    public boolean supports(AudioMedia media);
+    public boolean supports(AudioMedia media, Map<String,Object> properties);
 
     /**
      * Opens the media file. Performs startup code
      *
      * @param media
+     * @param properties - other properties requested to read the media
      * @throws IOException
      */
-    public void open(AudioMedia media) throws IOException;
+    public void open(AudioMedia media, Map<String,Object> properties) throws IOException;
 
     /**
-     * Reads one frame of the media.
-     *
-     * @param frame
-     * @return
-     * @throws IOException
+     * Reads one frame of the media
+     * 
+     * @return - the frame just read
+     * @throws IOException 
      */
     public Frame next() throws IOException;
 
@@ -43,9 +45,7 @@ public interface FrameIterator {
     public void close() throws IOException;
 
     /**
-     * Returns true if the end of the media is reached.
-     *
-     * @return
+     * @return <code>true</code> if the end of the media is reached.
      */
     public boolean hasNext();
     
