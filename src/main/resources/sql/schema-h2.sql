504dfec5d02930cddf032d38dc3ea5883d1cae4f
Carlos <Carlos@Carlos-PC>
2013-10-25 20:19:11 -0400

    validate if tables exist

--- src/main/resources/sql/schema-h2.sql
+++ src/main/resources/sql/schema-h2.sql
@@ -1,19 +1,15 @@
-DROP TABLE IF EXISTS stream_specs_audio_medias;
-DROP TABLE IF EXISTS audio_medias;
-DROP TABLE IF EXISTS stream_specs;
-
-CREATE TABLE stream_specs (
+CREATE TABLE IF NOT EXISTS stream_specs (
        name VARCHAR(32) NOT NULL
      , mountPoint VARCHAR(32) NOT NULL
      , PRIMARY KEY (name)
 );
 
-CREATE TABLE audio_medias (
+CREATE TABLE IF NOT EXISTS audio_medias (
        pathname VARCHAR(255) NOT NULL
      , PRIMARY KEY (pathname)
 );
 
-CREATE TABLE stream_specs_audio_medias (
+CREATE TABLE IF NOT EXISTS stream_specs_audio_medias (
        STREAM_SPECS_name VARCHAR(32) NOT NULL
      , audioMedias_pathname VARCHAR(255) NOT NULL
 );
